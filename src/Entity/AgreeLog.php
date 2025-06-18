30b13405d6fd8098ea1bcc2fcaf8334282619ffa
github-ci <you@example.com>
2025-06-18 10:32:54 +0800

    daily

--- src/Entity/AgreeLog.php
+++ src/Entity/AgreeLog.php
@@ -6,11 +6,9 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Tourze\DoctrineIndexedBundle\Attribute\IndexColumn;
 use Tourze\DoctrineSnowflakeBundle\Service\SnowflakeIdGenerator;
-use Tourze\DoctrineTimestampBundle\Attribute\CreateTimeColumn;
-use Tourze\DoctrineTimestampBundle\Attribute\UpdateTimeColumn;
+use Tourze\DoctrineTimestampBundle\Traits\TimestampableAware;
 use Tourze\EasyAdmin\Attribute\Column\ExportColumn;
 use Tourze\EasyAdmin\Attribute\Column\ListColumn;
-use Tourze\EasyAdmin\Attribute\Filter\Filterable;
 use UserAgreementBundle\Repository\AgreeLogRepository;
 
 #[ORM\Table(name: 'ims_member_protocol_agree_log', options: ['comment' => '接受协议日志'])]
@@ -18,6 +16,7 @@ use UserAgreementBundle\Repository\AgreeLogRepository;
 #[ORM\UniqueConstraint(name: 'member_protocol_agree_log_idx_uniq', columns: ['protocol_id', 'member_id'])]
 class AgreeLog
 {
+    use TimestampableAware;
     #[ExportColumn]
     #[ListColumn(order: -1, sorter: true)]
     #[ORM\Id]
@@ -36,21 +35,6 @@ class AgreeLog
 
     #[ORM\Column(type: Types::BOOLEAN, nullable: false, options: ['comment' => '同意', 'default' => 1])]
     private bool $valid = true;
-
-    #[Filterable]
-    #[IndexColumn]
-    #[ListColumn(order: 98, sorter: true)]
-    #[ExportColumn]
-    #[CreateTimeColumn]
-    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true, options: ['comment' => '创建时间'])]
-    private ?\DateTimeInterface $createTime = null;
-
-    #[UpdateTimeColumn]
-    #[ListColumn(order: 99, sorter: true)]
-    #[Filterable]
-    #[ExportColumn]
-    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true, options: ['comment' => '更新时间'])]
-    private ?\DateTimeInterface $updateTime = null;
 
     public function getId(): ?string
     {
@@ -89,25 +73,4 @@ class AgreeLog
     public function setValid(bool $valid): void
     {
         $this->valid = $valid;
-    }
-
-    public function setCreateTime(?\DateTimeInterface $createdAt): void
-    {
-        $this->createTime = $createdAt;
-    }
-
-    public function getCreateTime(): ?\DateTimeInterface
-    {
-        return $this->createTime;
-    }
-
-    public function setUpdateTime(?\DateTimeInterface $updateTime): void
-    {
-        $this->updateTime = $updateTime;
-    }
-
-    public function getUpdateTime(): ?\DateTimeInterface
-    {
-        return $this->updateTime;
-    }
-}
+    }}
